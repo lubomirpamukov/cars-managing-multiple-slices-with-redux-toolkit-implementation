040b166b6d1f6f0f0b9a2fc6093bdb4082f1a811
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-05-07 21:46:53 +0300

    added delete functionality

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,34 @@
-function CarList(){
-    return <div>Car List</div>
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar } from '../store'
+function CarList() {
+    const dispatch = useDispatch()
+  const cars = useSelector((state) => {
+    return state.cars.collection;
+  });
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id))
+  }
+  const renderedCars = cars.map((car) => {
+    return (
+      <div className="panel" key={car.id}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+  return <div className="car-list">
+    {renderedCars}
+    <hr />
+
+  </div>
 }
 
-export default CarList
+export default CarList;
