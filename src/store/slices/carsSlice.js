f2ffd64d301b3b81abd4e96a3af7f4d71fe50ba6
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-05-07 16:10:37 +0300

    implemented carSlice logic

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,32 @@
+ import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+ const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTerm: '',
+        cars: []
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            //action.payload === searchTerm
+            state.searchTerm = action.payload;
+        },
+        addCar(state,action) {
+            //action.payload = { name: Toyota auris, cost: 5000}
+            state.cars.push({
+                id: nanoid(),
+                name: action.payload.name,
+                cost: action.payload.cost
+            })
+        },
+        removeCar(state, action) {
+            //action.payload === car id
+            return state.filter((car) => {
+                car.id !== action.payload
+            })
+        }
+    }
+ });
+
+ export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+ export const { carsREducer } = carsSlice.reducer;
