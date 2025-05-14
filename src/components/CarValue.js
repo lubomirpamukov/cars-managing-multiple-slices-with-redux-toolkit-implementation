0bc5e178dcbe96efd349aa1a53cfba63bcd4767b
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-05-14 18:26:47 +0300

    added total car count functionality

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -2,20 +2,30 @@ import { useSelector } from "react-redux";
 import { createSelector } from "@reduxjs/toolkit";
 
 const selectTotalCost = createSelector(
-    [(state) => state.cars.collection, (state) => state.cars.searchTerm],
-    (collection, searchTerm) =>
-      collection
-        .filter((car) =>
-          car.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-        .reduce((acc, car) => acc + car.cost, 0)
-  );
+  [(state) => state.cars.collection, (state) => state.cars.searchTerm],
+  (collection, searchTerm) =>
+    collection
+      .filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, car) => acc + car.cost, 0)
+);
 
+const carCount = createSelector(
+  [(state) => state.cars.collection],
+  (collection) => collection.length
+);
 
 function CarValue() {
-  const totalCost = useSelector(selectTotalCost)
+  const totalCost = useSelector(selectTotalCost);
+  const totalCount = useSelector(carCount);
 
-  return <div className="car-value">Total Cost: ${totalCost}</div>;
+  return (
+    <>
+      <div className="car-value">Total Cost: ${totalCost}</div>
+      <div className="car-value">Total Count: {totalCount}</div>
+    </>
+  );
 }
 
 export default CarValue;
